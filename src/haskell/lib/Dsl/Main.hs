0a4cf4561572a3481e51027f7d97f6e8358dc86e
PawelWP <pawelwolak13@gmail.com>
2023-02-15 16:52:04 +0100

    CLI argument handling

--- src/haskell/lib/Dsl/Main.hs
+++ src/haskell/lib/Dsl/Main.hs
@@ -3,16 +3,18 @@ module Dsl.Main (main) where
 import qualified Dsl.NodeRunner as N
 import qualified Dsl.ParseNode as P
 import Prelude
+import System.Environment
 
 main :: IO ()
 main = do
-  putStrLn "Enter file name to parse: "
-  fileName <- getLine
-  putStrLn $ "Parsing " ++ show fileName
-  N.startNode fileName
-  file <- P.loadFileMock
-  putStrLn ""
+  arg <- getArgs
+  case arg of
+    (fileName : _ ) -> do 
+      putStrLn $ "Parsing " ++ show fileName
+      N.startNode fileName
+      file <- P.loadFileMock
+      putStrLn ""
   -- case file of
   --   Left err -> putStrLn $ show err
   --   Right res -> putStrLn $ show res
-  
+    _ -> putStrLn "unrecognized args"
