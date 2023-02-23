41aba68621ff703a6fa4e49bd55fffbcfd42a261
PawelWP <pawelwolak13@gmail.com>
2023-02-23 23:54:34 +0100

    handle error in node process

--- src/haskell/lib/Dsl/NodeRunner.hs
+++ src/haskell/lib/Dsl/NodeRunner.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Dsl.NodeRunner where 
 import Data.Aeson
 import System.Process
 import Prelude
+import Control.Exception
+import System.Exit 
 
 
 jsonDir :: String
@@ -13,8 +16,14 @@ startNode :: String -> IO ()
 startNode fileName  = do
   putStrLn "Running node process..."
   let com = "node ../node/parser.js " ++ fileName
-  output <- readCreateProcess (shell com ) {cwd = Just "/home/pawel/Desktop/watcher-dsl-beta/src/node"} []
-  writeFile jsonDir output
-  putStrLn $ output
-  putStrLn $ "Done."
+  output <- try $ readCreateProcess (shell com) {cwd = Just "/home/pawel/Desktop/watcher-dsl-beta/src/node"} []
+  case output of
+    Left (e :: SomeException) -> do
+      putStrLn $ "Error running Node.js process: " ++ show e
+      exitSuccess
+    Right output -> do
+      writeFile jsonDir output
+      putStrLn output
+      putStrLn "Done."
+
 
--- src/node/parser.js
+++ src/node/parser.js
@@ -14,5 +14,18 @@ const parserOptions = {
   project: path.join(__dirname, 'tsconfig.json'),
   range : true
 };
-const ast = parse(code, parserOptions);
-console.log(JSON.stringify(ast, null, 2));
+
+try {
+  const ast = parse(code, parserOptions);
+  console.log(JSON.stringify(ast, null, 2));
+} 
+catch (err) {
+  if (err.code === 'ENOENT') {
+    console.error(`Input file '${inputfile}' not found`);
+  } else if (err.message.includes('Unexpected token')) {
+    console.error(`Invalid syntax in input file '${inputfile}': ${err.message}`);
+  } else {
+    console.error(`Error while parsing code in input file '${inputfile}': ${err.message}`);
+  }
+
+}
