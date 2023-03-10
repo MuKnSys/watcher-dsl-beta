3df2b08ee97ebfacae8eea04df52312d0ce0a9b2
PawelWP <pawelwolak13@gmail.com>
2023-03-10 10:53:43 +0100

    catch error TS

--- src/node/parseToTs.js
+++ src/node/parseToTs.js
@@ -1,17 +1,15 @@
 const recast = require('recast');
 const fs = require('fs');
 const path = require('path');
-//const ts = require('typescript');
 
 const inputfile = process.argv[2];
-//const inputString = String(inputfile);
-// const ast = fs.readFileSync(filePath, 'utf-8');
-//const ast = JSON.stringify(inputString) 
-const parsedAst = JSON.parse(inputfile);
 
-//console.log("",  ast)
 
-const tsCode = recast.print(parsedAst, { parser: require('recast/parsers/typescript') }).code;
 
-console.log(tsCode)
-return tsCode;
+try {
+    const parsedAst = JSON.parse(inputfile);
+    const tsCode = recast.print(parsedAst, { parser: require('recast/parsers/typescript') }).code;
+    console.log(tsCode)
+} catch (err) {
+    console.error(err)
+}
