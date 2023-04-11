5bd27d43231bb7933b34be885d741912bd341129
Zoe Braiterman <zbraiterman@gmail.com>
2023-04-11 13:49:25 -0400

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -3,3 +3,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
 # Install NodeJS / npm
 sudo apt install npm
+
+# Clone the Watcher DSL repository
+git clone https://github.com/MuKnSys/watcher-dsl-beta.git
